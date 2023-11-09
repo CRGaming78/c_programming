1e8d13d92fd99bee62d3ecb195721c4da5241ac6
Aryan Kumar <81426042+CRGaming78@users.noreply.github.com>
2023-11-09 10:13:47 +0530

    Some edit on exp9.3.c

--- exp9.3.c
+++ exp9.3.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 union product {
     char name[50];
-    float price_per_unit;
-    int quantity_purchased;
-    float amount_spent;
+    float price;
+    int qty_purchased;
+    float amount;
 };
 int main() {
-    union product product_1;
+    union product product1;
     printf("Enter product name: ");
-    scanf("%s", product_1.name);
+    scanf("%s", product1.name);
     printf("Enter price per unit: ");
-    scanf("%f", &product_1.price_per_unit);
+    scanf("%f", &product1.price);
     printf("Enter number of quantities purchased: ");
-    scanf("%d", &product_1.quantity_purchased);
-    product_1.amount_spent = product_1.price_per_unit * product_1.quantity_purchased;
-    printf("\nProduct Details:\n");
-    printf("Name: %s\n", product_1.name);
-    printf("Price per unit: Rs%.2f\n", product_1.price_per_unit);
-    printf("Quantity purchased: %d\n", product_1.quantity_purchased);
-    printf("Amount spent: Rs%.2f\n", product_1.amount_spent);
+    scanf("%d", &product1.qty_purchased);
+    product1.amount_spent = product1.price*product1.qty_purchased;
+    printf("\nProduct Details:");
+    printf("Name: %s\n", product1.name);
+    printf("Price per unit: Rs%.2f\n", product1.price);
+    printf("Quantity purchased: %d\n", product1.qty_purchased);
+    printf("Amount spent: Rs%.2f\n", product1.amount_sp);    
     return 0;
 }
