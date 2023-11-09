cbac9df9281305ee7ed5bd25d479247dd043c753
Aryan Kumar <81426042+CRGaming78@users.noreply.github.com>
2023-11-09 10:19:01 +0530

    Some edit in exp9.1.c

--- exp9.1.c
+++ exp9.1.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 struct product {
     char name[50];
-    float price_per_unit;
-    int quantity_purchased;
-    float amount_spent;
+    float price;
+    int qty_purchased;
+    float amount;
 };
 int main() {
     struct product purchased_product;
     printf("Enter product name: ");
     scanf(" %s", purchased_product.name);
     printf("Enter price per unit: ");
-    scanf("%f", &purchased_product.price_per_unit);
+    scanf("%f", &purchased_product.price);
     printf("Enter number of quantities purchased: ");
-    scanf("%d", &purchased_product.quantity_purchased);
-    purchased_product.amount_spent = purchased_product.price_per_unit * purchased_product.quantity_purchased;
+    scanf("%d", &purchased_product.qty_purchased);
+    purchased_product.amount = purchased_product.price * purchased_product.qty_purchased;
     struct product *product_ptr = &purchased_product;
-    printf("\nProduct Details:\n");
+    printf("\nProduct Details:");
     printf("Name: %s\n", product_ptr->name);
-    printf("Price per unit: Rs%.2f\n", product_ptr->price_per_unit);
-    printf("Quantity purchased: %d\n", product_ptr->quantity_purchased);
-    printf("Amount spent: Rs%.2f\n", product_ptr->amount_spent);
+    printf("Price per unit: Rs%.2f\n", product_ptr->price);
+    printf("Quantity purchased: %d\n", product_ptr->qty_purchased);
+    printf("Amount spent: Rs%.2f\n", product_ptr->amount);
     return 0;
 }
